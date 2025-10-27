0dea31af69adb17427dcf26443b4fed232b0f694
briskt <3172830+briskt@users.noreply.github.com>
2025-10-27 19:53:39 +0800

    call Sentry\init during bootstrap

--- application/frontend/config/load-configs.php
+++ application/frontend/config/load-configs.php
@@ -8,6 +8,14 @@ require(__DIR__ . '/../../vendor/autoload.php');
 define('YII_ENV', Env::get('APP_ENV', 'prod'));
 define('YII_DEBUG', YII_ENV === 'dev' || YII_ENV === 'test');
 
+\Sentry\init([
+    'dsn' => getenv('SENTRY_DSN'),
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    'traces_sample_rate' => 1.0,
+]);
+
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 require(__DIR__ . '/../../common/config/bootstrap.php');
