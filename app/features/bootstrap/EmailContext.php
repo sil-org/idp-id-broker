a47a211c330bddc0fa56a45036c325802dc955c1
Mark Tompsett <mark_tompsett@sil.org>
2026-06-30 21:40:45 -0400

    [IDP-2171] Found the bug
    I mixed up the parameter names in the function.
    Once restored, all was good.

--- app/features/bootstrap/EmailContext.php
+++ app/features/bootstrap/EmailContext.php
@@ -66,8 +66,6 @@ class EmailContext extends YiiContext
 
     private ?EmailLog $tempEmailLog;
 
-    private string $fallback = '';
-
     public const METHOD_EMAIL_ADDRESS = 'method@example.com';
     public const MANAGER_EMAIL = 'manager@example.com';
     public const RECOVERY_EMAIL = 'recovery@example.com';
@@ -1215,7 +1213,7 @@ class EmailContext extends YiiContext
     #[Given('we are configured to CC :address as the mail admin fallback')]
     public function weAreConfiguredToCcAsTheMailAdminFallback($address): void
     {
-        $this->fallback = $address;
+        \Yii::$app->params['accountMailAdminsCcFallback'] = $address;
     }
 
     #[When('that user whose account has no mail admins is created')]
@@ -1224,7 +1222,6 @@ class EmailContext extends YiiContext
         Assert::null($this->tempUser, 'The user should not have existed yet.');
         // Unique address that still matches the "no mail admins" rule in the API mock.
         $email = str_replace('@', '_' . uniqid() . '@', self::NO_MAIL_ADMINS_EMAIL);
-        \Yii::$app->params['accountMailAdminsCcFallback'] = $this->fallback;
         $this->tempUser = $this->createNewUser(false, $email);
     }
 
