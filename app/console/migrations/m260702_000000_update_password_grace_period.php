693ff06658c074564a899d27828a5b000d682afc
Jason Jackson <jasonc.jackson@gmail.com>
2026-07-02 14:34:13 -0400

    migrate grace_period_ends_on

---
+++ app/console/migrations/m260702_000000_update_password_grace_period.php
@@ -0,0 +1,43 @@
+<?php
+
+use common\helpers\MySqlDateTime;
+use yii\db\Migration;
+use yii\db\Expression;
+
+class m260702_000000_update_password_grace_period extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $interval = MySqlDateTime::interval(\Yii::$app->params['passwordMfaLifespanExtension']);
+
+        $sql = "
+            UPDATE password
+            SET grace_period_ends_on = grace_period_ends_on {$interval}
+            WHERE
+                password.hibp_is_pwned = 'no'
+                AND EXISTS (SELECT 1 FROM mfa WHERE mfa.user_id = password.user_id AND mfa.verified = 1)
+        ";
+        $this->execute($sql);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $interval = MySqlDateTime::interval(\Yii::$app->params['passwordMfaLifespanExtension']);
+        $interval = MySqlDateTime::invertInterval($interval);
+
+        $sql = "
+            UPDATE password
+            SET grace_period_ends_on = grace_period_ends_on {$interval}
+            WHERE
+                password.hibp_is_pwned = 'no'
+                AND EXISTS (SELECT 1 FROM mfa WHERE mfa.user_id = password.user_id AND mfa.verified = 1)
+        ";
+        $this->execute($sql);
+    }
+}
