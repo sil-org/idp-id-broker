cf4c609c4a1253d3bc90c922f55e47124f1c6ece
Jason Jackson <jasonc.jackson@gmail.com>
2026-07-02 14:58:34 -0400

    Update down with note on rolling back

--- app/console/migrations/m260702_000000_update_password_grace_period.php
+++ app/console/migrations/m260702_000000_update_password_grace_period.php
@@ -28,16 +28,7 @@ class m260702_000000_update_password_grace_period extends Migration
      */
     public function safeDown()
     {
-        $interval = MySqlDateTime::interval(\Yii::$app->params['passwordGracePeriodExtension']);
-        $interval = MySqlDateTime::invertInterval($interval);
-
-        $sql = "
-            UPDATE password
-            SET grace_period_ends_on = expires_on {$interval}
-            WHERE
-                password.hibp_is_pwned = 'no'
-                AND EXISTS (SELECT 1 FROM mfa WHERE mfa.user_id = password.user_id AND mfa.verified = 1)
-        ";
-        $this->execute($sql);
+        // Can't safely roll back this one until m260527_000000_update_password_expiration_grace_period
+        // is rolled back, need to update the expires on, then grace_period_ends_on can be updated again
     }
 }
