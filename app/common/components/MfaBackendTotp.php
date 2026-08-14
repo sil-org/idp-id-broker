3a7a5ffd2a147a4318c49ae36bfbf8858d5e5760
Ethan Cannelongo <ethancannelongo@gmail.com>
2026-08-14 15:44:42 -0400

    Fix variable name for clarity

--- app/common/components/MfaBackendTotp.php
+++ app/common/components/MfaBackendTotp.php
@@ -50,8 +50,8 @@ class MfaBackendTotp extends Component implements MfaBackendInterface
             throw new NotFoundHttpException("User not found when trying to create new TOTP configuration");
         }
 
-        $idpName = \Yii::$app->params['idpDisplayName'];
-        return $this->client->createTotp($user->email, $idpName);
+        $idpDisplayName = \Yii::$app->params['idpDisplayName'];
+        return $this->client->createTotp($user->email, $idpDisplayName);
     }
 
     /**
